9433121eb7f78a2c097d1d04d6e68dbca9eaca58
Coutinho <lcfreitas@inf.ufpel.edu.br>
2021-04-27 00:05:28 -0300

    hotfix: fix no reference to utils

--- torchmetagen/models/utils.py
+++ torchmetagen/models/utils.py
@@ -3,29 +3,28 @@ from keras.layers.convolutional import Conv1D
 import torch
 import numpy as np
 
-
 try:
     from torch.hub import load_state_dict_from_url
 except ImportError:
     from torch.utils.model_zoo import load_url as load_state_dict_from_url
-	
+
+
 
 def keras_to_pyt(km, pm=None):
-    r"""
-    Convert pretrained keras models to torch
+	r"""
+	Convert pretrained keras models to torch
 
-    Args:
-        km                     : pretrained keras model
-        [Optional] pm          : torch model 
-    
-    Returns:
-        Union[nn.Module, Dict] : transloaded torch model OR state dict
-    """
+	Args:
+		km                     : pretrained keras model
+		[Optional] pm          : torch model 
 
-	weight_dict = dict()
+	Returns:
+		Union[nn.Module, Dict] : transloaded torch model OR state dict
+	"""
+	weight_dict = {}
 	for layer in km.layers:
 		if (type(layer) is Conv1D):
-            weight_dict[layer.get_config()['name'] + '.weight'] = np.transpose(layer.get_weights()[0], ( 2, 1, 0 ))
+			weight_dict[layer.get_config()['name'] + '.weight'] = np.transpose(layer.get_weights()[0], ( 2, 1, 0 ))
 			weight_dict[layer.get_config()['name'] + '.bias'] = layer.get_weights()[1]
 		elif type(layer) is keras.layers.Dense:
 			weight_dict[layer.get_config()['name'] + '.weight'] = np.transpose(layer.get_weights()[0], (1, 0))
